780fcb6c3c360b050834bbd0e7350f4e4144cf17
ZH <zhanghan@gmx.cn>
2019-11-04 16:17:19 +0800

    Updated to work with target devices

--- SPF.ino
+++ SPF.ino
@@ -1,21 +1,23 @@
-static long m_baudrate = 115200;
+static long m_baudrate = 4800;
 static uint8_t m_config = SERIAL_8N1;
 
-void setup() {
-    // Serial.begin(m_baudrate, m_config);
+static long console_baudrate = 115200;
+static uint8_t console_config = SERIAL_8N1;
 
-    Serial1.begin(m_baudrate, m_config);
-    Serial2.begin(m_baudrate, m_config);
-    Serial3.begin(m_baudrate, m_config);
+void setup() {
+    Serial3.begin(console_baudrate, console_config);
 
     Serial3.println("");
-    Serial3.println("Serial Port Forwarder (v0.1)");
+    Serial3.println("Serial Port Forwarder (v0.2)");
     Serial3.println("====================================");
-    Serial3.println("2019-11-01, Eric Zhang <Eric.ZH.Zhang@emerson.com>");
+    Serial3.println("2019-11-04, Eric Zhang <Eric.ZH.Zhang@emerson.com>");
     Serial3.println("");
     Serial3.println("Bridges Serial1 and Serial2, and forward them to Serial3");
     Serial3.println("--------------------------------------------");
     Serial3.println("");
+    
+    Serial1.begin(m_baudrate, m_config);
+    Serial2.begin(m_baudrate, m_config);
 }
 
 void serialEvent1()

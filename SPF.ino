cd05023e4982a8e38aee42f3b97eae2920227477
ZH <zhanghan@gmx.cn>
2019-11-05 12:45:12 +0800

    Added a definition to select monitor port

--- SPF.ino
+++ SPF.ino
@@ -4,17 +4,20 @@ static uint8_t m_config = SERIAL_8N1;
 static long console_baudrate = 115200;
 static uint8_t console_config = SERIAL_8N1;
 
-void setup() {
-    Serial3.begin(console_baudrate, console_config);
+static HardwareSerial& serialPort = Serial;
 
-    Serial3.println("");
-    Serial3.println("Serial Port Forwarder (v0.2)");
-    Serial3.println("====================================");
-    Serial3.println("2019-11-04, Eric Zhang <Eric.ZH.Zhang@emerson.com>");
-    Serial3.println("");
-    Serial3.println("Bridges Serial1 and Serial2, and forward them to Serial3");
-    Serial3.println("--------------------------------------------");
-    Serial3.println("");
+void setup() {
+    serialPort.begin(console_baudrate, console_config);
+
+    serialPort.println("");
+    serialPort.println("Serial Port Forwarder (v0.2)");
+    serialPort.println("====================================");
+    serialPort.println("Eric Zhang <Eric.ZH.Zhang@emerson.com>");
+    serialPort.println("2019-11-05");
+    serialPort.println("");
+    serialPort.println("Bridges Serial1 and Serial2, and forward them to Serial or Serial3");
+    serialPort.println("--------------------------------------------");
+    serialPort.println("");
     
     Serial1.begin(m_baudrate, m_config);
     Serial2.begin(m_baudrate, m_config);
@@ -26,7 +29,7 @@ void serialEvent1()
     {
         register char c = Serial1.read();
         Serial2.write(c);
-        Serial3.write(c);
+        serialPort.write(c);
     }
 }
 
@@ -36,7 +39,7 @@ void serialEvent2()
     {
         register char c = Serial2.read();
         Serial1.write(c);
-        Serial3.write(c);
+        serialPort.write(c);
     }
 }
 
